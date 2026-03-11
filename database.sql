0daae5b6b919b025a2400e156f972d29d7d95f21
CarlosChelle73 <carloschelle68@gmail.com>
2026-03-10 21:36:58 -0300

    Create database.sql
    Tabela criada

---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE FirstStepDev;
+
+#####TABELA INICIANTES#####
+
+CREATE TABLE Iniciantes (
+id_iniciantes INT AUTO_INCREMENT PRIMARY KEY,
+nivel_experiencia varchar(2),
+pontuacao int,
+bio text
+
+);
